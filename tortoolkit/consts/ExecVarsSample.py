50733585b8e23a4cbc43388aabb14b54dbcdb1e5
Botmirror69 <92679061+Botmirror69@users.noreply.github.com>
2022-05-16 12:47:43 +0530

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -5,20 +5,20 @@ except:
         # Set true if its VPS
         IS_VPS = False
         
-        API_HASH = ""
-        API_ID = 0
-        BOT_TOKEN = ""
-        BASE_URL_OF_BOT = ""
+        API_HASH = "1264d2d7d397c4635147ee25ab5808d1"
+        API_ID = 3477714
+        BOT_TOKEN = "2071886190:AAGEKJ9QDlHvwcOESD_GIBoX6QBgSVi25DA"
+        BASE_URL_OF_BOT = "https://google.com"
 
         # Edit the server port if you want to keep it default though.
         SERVPORT = 80
 
         # ALLOWED USERS [ids of user or supergroup] seperate by commas
-        ALD_USR = []
-        OWNER_ID = 0
+        ALD_USR = [1979647570 1801012395 1773311819]
+        OWNER_ID = 1773311819
         
         # Google Drive Index Link should include the base dir also See readme for more info
-        GD_INDEX_URL = False
+        GD_INDEX_URL = https://bold-silence-0eed.shikari.workers.dev/0:/
 
         # Time to wait before edit message
         EDIT_SLEEP_SECS = 40
@@ -36,7 +36,7 @@ except:
         REMAINING_STR = "▱"
 
         # DB URI for access
-        DB_URI = "dbname=tortk user=postgres password=your-pass host=127.0.0.1 port=5432"
+        DB_URI = "postgres://zfmrklxqreloig:d0ea65edbad27dc80c0bb0d3bddab854a1f71e49e3af0ad2941ba3d2c96de65f@ec2-44-196-223-128.compute-1.amazonaws.com:5432/deln4phs2oaihe"
         
         # UNCOMMENT THE BELOW LINE WHEN USING CONTAINER AND COMMENT THE UPPER LINE
         #DB_URI = "dbname=tortk user=postgres password=your-pass host=db port=5432"
@@ -56,7 +56,7 @@ except:
 
         # Will be enabled once its set
         # For vps change it to True if config loaded
-        RCLONE_ENABLED = False
+        RCLONE_ENABLED = True
 
         # If the user fails to select whether to use rclone or telegram to upload this will be the deafult.
         DEFAULT_TIMEOUT = "leech"
@@ -65,26 +65,26 @@ except:
         RCLONE_CONFIG = False
         
         # Name of the RCLONE drive from the config
-        DEF_RCLONE_DRIVE = ""
+        DEF_RCLONE_DRIVE = "shikari-mirror"
 
         # Max size of a playlist that is allowed (Number of videos)
         MAX_YTPLAYLIST_SIZE = 20
         
         # Max size of the torrent allowed
-        MAX_TORRENT_SIZE = 10
+        MAX_TORRENT_SIZE = 0
 
         # Set this to your bot username if you want to add the username of your bot at the end of the commands like
         # /leech@TorToolkitBot so the value will be @TorToolkitBot
-        BOT_CMD_POSTFIX = "" 
+        BOT_CMD_POSTFIX = "@shikari_mirror_bot" 
 
         # Time out for the status Delete.
-        STATUS_DEL_TOUT = 20
+        STATUS_DEL_TOUT = -1
 
         # Allow the user settings to be accessed in private
         USETTINGS_IN_PRIVATE = False
 
         # Torrent max time to collect metadata in seconds
-        TOR_MAX_TOUT = 180
+        TOR_MAX_TOUT = 1
 
         # This is to stop someone from abusing the system by imposing the limit
         # [<GBs of total torrent sapce>, <Number of youtube videos allowed to download>, <Number of youtube playlists allowed to download>]
