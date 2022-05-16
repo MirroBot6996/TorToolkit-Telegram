f1b77b1eb64967f80b0b064f6902d572edbc614d
MirroBot6996 <104616026+MirroBot6996@users.noreply.github.com>
2022-05-16 13:51:17 +0530

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -14,7 +14,7 @@ except:
         SERVPORT = 80
 
         # ALLOWED USERS [ids of user or supergroup] seperate by commas
-        ALD_USR = [1979647570 1801012395 1773311819]
+        ALD_USR = [1979647570, 1801012395, 1773311819, -1001593432602]
         OWNER_ID = 1773311819
         
         # Google Drive Index Link should include the base dir also See readme for more info
